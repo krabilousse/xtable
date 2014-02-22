7913eee61976a0a5eba8f5ecb818a0f479b2279d
damianorenfer <damiano.renfer@he-arc.ch>
2014-02-22 17:20:33 +0100

    Intégration de devise et omniauth-facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Xtable::Application.routes.draw do
+  root to: "staticcontent#home"
+  
+  get "/private" => "staticcontent#private_content", as: "private_content" 
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
