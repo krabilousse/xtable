4fbc419c88eec0ac71c553c9c2e0129d9e2d7b21
damianorenfer <damiano.renfer@he-arc.ch>
2014-02-25 17:07:38 +0100

    Generation of entities

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Xtable::Application.routes.draw do
+  resources :groups
+
+  resources :events
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
