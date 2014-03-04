b584a5272ce4c9cb9e21a67124be75008cdc0911
Danick Fort <danick.fort@he-arc.ch>
2014-03-04 16:08:47 +0100

    Devise sign in form customization

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
   before_action :set_group, only: [:show, :edit, :update, :destroy]
-
+  # before_filter :authenticate_user!
   # GET /groups
   # GET /groups.json
   def index
