bbd397aa98a03e5314c510ae49a0b3b6065670d7
damianorenfer <damiano.renfer@he-arc.ch>
2014-03-04 16:46:20 +0100

    Removed registrations routes except registrations#destroy

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
+         :rememberable, :trackable, :validatable,
          :omniauthable, :omniauth_providers => [:facebook]
          
   def self.find_for_facebook_oauth(auth)
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,11 @@ Xtable::Application.routes.draw do
   root to: "staticcontent#home"
   
   get "/private" => "staticcontent#private_content", as: "private_content" 
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }, skip: :registrations
+  
+  as :user do    
+    delete 'users' => 'devise/registrations#destroy', :as => 'delete_user_registration'
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
