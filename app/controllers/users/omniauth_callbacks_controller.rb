802cfd1e99994443fbdd54f9bf0b0298e0ce1c86
damianorenfer <damiano.renfer@he-arc.ch>
2014-04-04 15:13:49 +0200

    Updated home (welcome screen) page and global layout

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,6 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   skip_before_filter :authenticate_user!
+  
   def all
     p env["omniauth.auth"]
     user = User.from_omniauth(env["omniauth.auth"])
