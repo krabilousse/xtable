450155b07812ae03bc7315b1c4b2209e52758fcc
Mirco Nasuti <mirco.nasuti@he-arc.ch>
2014-04-11 15:10:51 +0200

    modified get admin stuff

--- app/models/group.rb
+++ app/models/group.rb
@@ -24,7 +24,7 @@ class Group < ActiveRecord::Base
   
   def add_admin(user)
     begin
-      self.add_user(user, "Admin")     
+      self.add_user(user, "Admin")
       true      
     rescue
       false
@@ -36,5 +36,10 @@ class Group < ActiveRecord::Base
     ur = UserRole.create(user: user, role: role, group: self)   
   end
   
+  def get_admin
+    adminRole = Role.where(name: 'Admin').first
+    ur = UserRole.where(role: adminRole, group: self).first
+    ur.user    
+  end
     
 end
